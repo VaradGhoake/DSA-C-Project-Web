d0161105493ed86b9934eddd78568f83b6937d33
Varad Ghodake <varadghodake@gmail.com>
2016-08-12 11:25:26 +0530

    Add files via upload

---
+++ wget.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+int main() {
+    system("wget --proxy-user 111503022 --proxy-password ghodakevv15.comp_111503022 http://www.twitter.com/");
+    system("clear");
+    system("firefox index.html.12");
+    return 0;
+}
