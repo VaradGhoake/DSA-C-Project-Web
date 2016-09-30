344ebcc2097931daff0703da60cfd4f9a0cd41f4
Varad Ghodake <varadghodake@gmail.com>
2016-09-30 09:18:02 +0530

    Delete wget.c

--- wget.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-int main() {
-    system("wget --proxy-user 111503022 --proxy-password ghodakevv15.comp_111503022 http://www.twitter.com/");
-    system("clear");
-    system("firefox index.html.12");
-    return 0;
-}
