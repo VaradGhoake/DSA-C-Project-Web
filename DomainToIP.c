7d422365dc4080b91d77157fb4ab0d8ccc26df32
Varad Ghodake <varadghodake@gmail.com>
2016-10-01 20:30:22 +0530

    Update DomainToIP.c

--- DomainToIP.c
+++ DomainToIP.c
@@ -3,41 +3,51 @@ char* DomainToIP(char *domain) {
 
 	struct addrinfo hints, *res, *p;
 	int status;
-	char ipstr[INET6_ADDRSTRLEN];
+
+	printf("Resolving URL...\n");
+	unsigned int retTime = time(0) + 2;
+    	while (time(0) < retTime);    
 
 
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+	/*************************************Fail Cases********************************************/
 
+
+	if ((status = getaddrinfo(domain, NULL, &hints, &res)) != 0) {
+		printf("Null");
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        	return 2;
+        	return NULL;
     	}
 
-    	//printf("IP addresses for %s:\n\n", argv[1]);
-
+    	
     	for(p = res; p != NULL; p = p->ai_next) {
         	void *addr;
         	char *ipver;
 
-        // get the pointer to the address itself,
-        // different fields in IPv4 and IPv6:
-        	if (p->ai_family == AF_INET) { // IPv4
+        /**********************getting the pointer to the address itself***************************/
+        // different fields in IPv4 and IPv6
+
+
+	/**********************************Version detection ipv4 or ipv6************************/
+
+        	if (p->ai_family == AF_INET) {
         		struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
         		addr = &(ipv4->sin_addr);
         		ipver = "IPv4";
  
 
-       		} else { // IPv6
+       		} else {
         		struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
         		addr = &(ipv6->sin6_addr);
         		ipver = "IPv6";
         }
 
-        // convert the IP to a string and print it:
- 	       inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
- 	       return ipstr;
+        /***************************converting the IP to a string **************************************/
+
+ 	       inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr); 
+	       return ipstr;
     }
 }
