88e22051c82782c16086dedd8c391e47735fa75b
Varad Ghodake <varadghodake@gmail.com>
2016-10-01 20:33:07 +0530

    Update headers.h

--- headers.h
+++ headers.h
@@ -1,4 +1,11 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <time.h>
+
+char ipstr[INET6_ADDRSTRLEN];
